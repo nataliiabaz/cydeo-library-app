f9053f4de7fab8d2475aaf6ba115bfd226fae647
Nataliia Bazel <bazelnataly@gmail.com>
2022-11-12 14:19:00 -0600

    changes added in cukesRunner

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
             },
             features = "src/test/resources/features",
             glue="com/cydeo/library/step_definitions",
-            dryRun = true,
+            publish = true,
             tags = " smoke"
 
     )
